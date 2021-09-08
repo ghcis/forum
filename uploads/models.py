054b8a3a78517f7c6133c2a67a37f142445a35e6
汪心禾 <wangxinhe06@gmail.com>
2021-09-09 07:34:56 +0800

    Uploads demo

--- uploads/models.py
+++ uploads/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file = models.FileField()
+
+    def __str__(self):
+        return str(self.file)
--- uploads/views.py
+++ uploads/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import UploadFileForm
 
-# Create your views here.
+
+def upload_file(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # file is saved
+            form.save()
+            return HttpResponseRedirect("/success/url/")
+    else:
+        form = UploadFileForm()
+    return render(request, "uploads/upload.html", {"form": form})
