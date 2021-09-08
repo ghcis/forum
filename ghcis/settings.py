6f6276c3fe3fcaacc106c2ea4648cc384c2f790c
汪心禾 <wangxinhe06@gmail.com>
2021-09-09 07:33:49 +0800

    Update hosts and L10n settings

--- ghcis/settings.py
+++ ghcis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ghcis.piterator.com"]
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "PRC"
 
 USE_I18N = True
 
