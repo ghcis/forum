f026e316404a35087136a8365943d308bfbcc8e2
汪心禾 <wangxinhe06@gmail.com>
2021-09-11 17:57:20 +0800

    Bootstrap

--- ghcis/settings.py
+++ ghcis/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [BASE_DIR / "node_modules"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
