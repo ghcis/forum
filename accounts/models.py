35c10ae373b7af710b10fc63d0b9d6c684d54836
汪心禾 <wangxinhe06@gmail.com>
2021-10-08 17:21:04 +0800

    Update migrations

--- accounts/models.py
+++ accounts/models.py
@@ -11,8 +11,8 @@ class Profile(models.Model):
     user = models.OneToOneField(
         User, verbose_name="用户", on_delete=models.CASCADE
     )
-    bio = models.TextField("个人简介", null=True, blank=True)
     avatar = models.ImageField("头像", upload_to=avatar_directory_path)
+    bio = models.TextField("个人简介", null=True, blank=True)
 
     def __str__(self) -> str:
         return str(self.user)
